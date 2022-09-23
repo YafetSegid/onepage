b76ec6b7214276d5801e1aa05032ee5305b5d5fb
Yafet Segid <>
2022-09-23 12:56:48 -0500

    five

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
- <h1>This is one</h1>
- <h1>This is one</h1>
+
+     <h1>This is one</h1>
+    <p>Which one is this!!</p>
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+    <p>4</p>
+    <p>5</p>
     </div>
-  );
+  )
 }
 
 export default App;
